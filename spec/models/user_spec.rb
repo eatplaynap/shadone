253af7a1f33bd0852820e995427b3e45184ad72f
eatplaynap <eatplaynap329@gmail.com>
2022-05-13 17:17:33 +0900

    rubocop OK

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid without a name" do
+  it 'is invalid without a name' do
     user = User.new(name: nil)
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
