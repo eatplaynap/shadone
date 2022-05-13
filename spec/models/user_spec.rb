d4fb44bc3beee84d4a573448af5ea4c2f476931b
eatplaynap <eatplaynap329@gmail.com>
2022-05-13 17:16:51 +0900

    add name validation test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
 end
