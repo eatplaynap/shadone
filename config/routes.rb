a37ca46940dd1515387ec7acc94bd1f28e37d5cc
eatplaynap <eatplaynap329@gmail.com>
2022-05-26 17:31:45 +0900

    Create new route to POST `/api/practices`

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :users, only: [:show, :edit, :destroy]
   resources :practices, only: [:new, :show, :edit, :destroy]
+  namespace :api do
+    resources :practices, only: :create
+  end
 end
