728ff9334cd7fa257362034bfdb2b5cfa3dd648f
eatplaynap <eatplaynap329@gmail.com>
2022-05-27 17:01:48 +0900

    Assign id on the page where Vue app to appear to render the Vue app only when the HTML element with that id is found

--- app/javascript/youtube-loop.js
+++ app/javascript/youtube-loop.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './youtube-loop.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: (h) => h(App),
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
+  const selector = '#js-youtube-loop'
+  const youtubeLoop = document.querySelector(selector)
+  if (youtubeLoop) {
+    const app = new Vue({
+      render: (h) => h(App),
+    }).$mount(selector)
+    document.body.appendChild(app.$el)
+  }
 })
