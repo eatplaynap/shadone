003d17484ae4450ed93b2c27b4a1bf8fcc09ebf9
eatplaynap <eatplaynap329@gmail.com>
2022-06-12 15:16:10 +0900

    Add previous month

--- app/javascript/practice-calendar.vue
+++ app/javascript/practice-calendar.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="practiceCalendar">
-    <p>{{ currentYear }}年{{ currentMonth }}月</p>
+    <div @click="previousMonth">Previous Month</div>
+    <div>{{ calendarYear }} - {{ calendarMonth }}</div>
+    <div v-if="!newestMonth()" @click="nextMonth">Next Month</div>
     <table>
       <thead>
-      <tr>
-        <th>Su</th>
-        <th>Mo</th>
-        <th>Tu</th>
-        <th>We</th>
-        <th>Th</th>
-        <th>Fr</th>
-        <th>Sa</th>
-      </tr>
+        <tr>
+          <th>Su</th>
+          <th>Mo</th>
+          <th>Tu</th>
+          <th>We</th>
+          <th>Th</th>
+          <th>Fr</th>
+          <th>Sa</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="week in monthlyCalendar" :key="week.id">
-        <td v-for="date in week.value" :key="date.key">{{ date.date }}</td>
-      </tr>
+        <tr v-for="week in monthlyCalendar" :key="week.id">
+          <td v-for="date in week.value" :key="date.key">{{ date.date }}</td>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -28,6 +30,8 @@ export default {
     return {
       currentYear: this.getCurrentYear(),
       currentMonth: this.getCurrentMonth(),
+      calendarYear: this.getCurrentYear(),
+      calendarMonth: this.getCurrentMonth(),
       firstWday: this.getFirstWday(),
       lastDate: this.getLastDate(),
       monthlyCalendar: [],
@@ -45,11 +49,11 @@ export default {
       return new Date().getMonth() + 1
     },
     getFirstWday() {
-      const firstDay = new Date(this.getCurrentYear(), this.getCurrentMonth() - 1,1)
+      const firstDay = new Date(this.calendarYear, this.calendarMonth - 1, 1)
       return firstDay.getDay()
     },
     getLastDate() {
-      const lastDay = new Date(this.getCurrentYear(), this.getCurrentMonth(),0)
+      const lastDay = new Date(this.calendarYear, this.calendarMonth, 0)
       return lastDay.getDate()
     },
     getMonthlyCalendar() {
@@ -57,7 +61,7 @@ export default {
       let key = 0
       let id = 1
       if (this.getFirstWday() >= 1) {
-        for(let blank = 0; blank < this.getFirstWday(); blank++) {
+        for (let blank = 0; blank < this.getFirstWday(); blank++) {
           value.push({ key: key })
           key++
         }
@@ -72,7 +76,33 @@ export default {
           key = 0
         }
       }
-    }
-  }
+    },
+    previousMonth() {
+      if (this.calendarMonth === 1) {
+        this.calendarMonth = 12
+        this.calendarYear--
+      } else {
+        this.calendarMonth--
+      }
+      this.monthlyCalendar = []
+      this.getMonthlyCalendar()
+    },
+    nextMonth() {
+      if (this.calendarMonth === 12) {
+        this.calendarMonth = 1
+        this.calendarYear++
+      } else {
+        this.calendarMonth++
+      }
+      this.monthlyCalendar = []
+      this.getMonthlyCalendar()
+    },
+    newestMonth() {
+      return (
+        this.currentYear === this.calendarYear &&
+        this.currentMonth === this.calendarMonth
+      )
+    },
+  },
 }
 </script>
