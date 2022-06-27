ec4d07a3ef3e1fbe133ee843e5bc5b9f9738dcfc
eatplaynap <eatplaynap329@gmail.com>
2022-06-27 11:47:16 +0900

    Implement current_user related methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def current_user=(user)
+    session[:user_id] = user.id
   end
 end
