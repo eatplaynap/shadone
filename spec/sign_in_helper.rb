bb73077446abee3000ff9061486fe9d17f4b2c0a
eatplaynap <eatplaynap329@gmail.com>
2022-06-28 17:00:07 +0900

    Create sign in module for login test

---
+++ spec/sign_in_helper.rb
@@ -0,0 +1,16 @@
+module SignInHelper
+  def sign_in_as(user)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock(
+      user.provider,
+      uid: user.uid,
+      info: { name: user.name })
+    visit welcome_url
+    click_on 'Login'
+    @current_user = user
+  end
+
+  def current_user
+    @current_user
+  end
+end
