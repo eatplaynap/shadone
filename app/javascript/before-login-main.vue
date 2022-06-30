03af623d7b7932de29438c0a3c9329d8ee4ad206
eatplaynap <eatplaynap329@gmail.com>
2022-06-30 15:58:18 +0900

    Create before login main page

---
+++ app/javascript/before-login-main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <YouTubeLoop />
+  </div>
+</template>
+
+<script>
+import YouTubeLoop from './youtube-loop.vue'
+
+export default {
+  components: {
+    YouTubeLoop,
+  },
+}
+</script>
