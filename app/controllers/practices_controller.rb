6f8cc5cd8e6938e7a31ede0eda1f900f47ddc013
eatplaynap <eatplaynap329@gmail.com>
2022-07-01 16:15:55 +0900

    Add practice edit page

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -7,7 +7,22 @@ class PracticesController < ApplicationController
     @practice = Practice.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    @practice = Practice.find(params[:id])
+  end
+
+  def update
+    practice = Practice.find(params[:id])
+    practice.update!(practice_params)
+    redirect_to practice_url, notice: "Updated practice#{practice.id}"
+  end
 
   def destroy; end
+
+  private
+
+  def practice_params
+    params.require(:practice).permit(:url, :duration, :memo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users, only: [:show, :edit, :destroy]
-  resources :practices, only: [:new, :show, :edit, :destroy]
+  resources :practices, only: [:new, :show, :edit, :update, :destroy]
   namespace :api do
     resources :practices, only: [:index, :create]
   end
