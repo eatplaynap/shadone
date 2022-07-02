02ee9ada35034a37baa49d636e5b1ac50723ddb1
eatplaynap <eatplaynap329@gmail.com>
2022-07-02 22:01:31 +0900

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show; end
 
-  def edit; end
+  def show
+    @user = current_user
+  end
 
   def destroy; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'top#index'
-  get "welcome", to: "welcome#index", as: "welcome"
-  get "auth/:provider/callback", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  resources :users, only: [:show, :edit, :destroy]
-  resources :practices, only: [:new, :show, :edit, :update, :destroy]
+  root "top#index"
+  get 'welcome', to: 'welcome#index', as: 'welcome'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/me', to: 'users#show'
+  delete '/me', to: 'users#destroy'
+  resources :practices, only: [:show, :edit, :update, :destroy]
   namespace :api do
     resources :practices, only: [:index, :create]
   end
