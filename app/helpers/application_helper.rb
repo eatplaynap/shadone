410e329de948cf61e3f1cf1879aaf9cc43c2bb7f
eatplaynap <eatplaynap329@gmail.com>
2022-07-04 17:52:56 +0900

    Implement total practice hours column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def seconds_to_str(seconds)
+    ["#{seconds / 3600}h", "#{seconds / 60 % 60}m", "#{seconds % 60}s"]
+      .select { |str| str =~ /[1-9]/ }.join(' ')
+  end
 end
