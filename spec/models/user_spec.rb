6fcfa86d238b6bac9aa1bc91f80ef6d310b8e4d9
eatplaynap <eatplaynap329@gmail.com>
2022-07-06 13:50:29 +0900

    Remove User model's validation test along with the removal of its validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is invalid without a name' do
-    user = User.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-  end
-
   describe '.find_or_create_from_auth_hash!' do
   end
 
