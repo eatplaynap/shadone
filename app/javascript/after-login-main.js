ac7eb66a5ac02350e848871a0be24a4a03a5e572
eatplaynap <eatplaynap329@gmail.com>
2022-07-06 11:57:40 +0900

    Change event passed to addEventListener to display Vue components when turbolinks is loaded

--- app/javascript/after-login-main.js
+++ app/javascript/after-login-main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './after-login-main.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const selector = '#js-main'
   const main = document.querySelector(selector)
   if (main) {
