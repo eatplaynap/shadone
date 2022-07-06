bbdbcbba1898d7398817cd36533d9d4e97a2c691
eatplaynap <eatplaynap329@gmail.com>
2022-07-06 12:01:12 +0900

    Rough sketch of entire tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
+
+  describe '.find_or_create_from_auth_hash!' do
+
+  end
+
+  describe '#total_practice_duration' do
+    # あるユーザーに対するプラクティスが作成されていたらそのdurationの合計をテスト
+    # なにもないときは0
+  end
 end
--- spec/system/welcome_spec.rb
+++ spec/system/welcome_spec.rb
@@ -3,28 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  # let(:user) { FactoryBot.create(:user) }
+  #
+  # it 'can log in' do
+  #   sign_in_as user
+  #   # サインインしましたをチェック
+  #   expect(page).to have_content 'Logout'
+  # end
+  #
+  # it 'can log out' do
+  #   sign_in_as user
+  #   click_link 'Logout'
+  #
+  #   expect(page).to have_content 'Login'
+  # end
+  #
+  # it 'cannot access / when user was not logged in' do
+  #   visit root_path
+  #   # /welcomeに戻ることをテスト
+  #   expect(page).to have_content 'Login'
+  # end
+  #
+  # it 'can access / when user was logged in' do
+  #   sign_in_as user
+  #   # ログイン後root_pathにいることをテスト
+  #   visit root_path
+  #   expect(page).to have_content 'Calendar'
+  # end
 
-  it 'can log in' do
-    sign_in_as user
-    expect(page).to have_content 'Logout'
-  end
+  it 'can do loop' do
 
-  it 'can log out' do
-    sign_in_as user
-    click_link 'Logout'
-
-    expect(page).to have_content 'Login'
-  end
-
-  it 'cannot access / when user was not logged in' do
-    visit root_path
-    expect(page).to have_content 'Login'
-  end
-
-  it 'can access / when user was logged in' do
-    sign_in_as user
-    visit root_path
-    expect(page).to have_content 'Calendar'
   end
 end
