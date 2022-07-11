2c510c82adab508023aae3dacc0f566880564b5b
eatplaynap <eatplaynap329@gmail.com>
2022-07-11 17:58:52 +0900

    Install tailwind

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,25 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+function hotfixPostcssLoaderConfig (subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+}
+
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
 module.exports = environment
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/views/**/*.html.slim',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
