18523f671222bdc0ca21df37c26b495323ed2d94
eatplaynap <eatplaynap329@gmail.com>
2022-07-14 18:03:00 +0900

    Enable to access older/newer log page from a show page

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -5,4 +5,12 @@ class Practice < ApplicationRecord
   validates :url, presence: true
   validates :duration, presence: true
   validates :practice_on, presence: true, uniqueness: { scope: :user_id }
+
+  def previous
+    Practice.where(user: user).where('practice_on < ?', practice_on).order(practice_on: :desc).first
+  end
+
+  def next
+    Practice.where(user: user).where('practice_on > ?', practice_on).order(:practice_on).first
+  end
 end
