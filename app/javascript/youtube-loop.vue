b34f6af36c464d0f764b70a57566572caa03c5df
eatplaynap <eatplaynap329@gmail.com>
2022-07-14 16:11:39 +0900

    Disable to popup toast when user doesn't logged in

--- app/javascript/youtube-loop.vue
+++ app/javascript/youtube-loop.vue
@@ -46,11 +46,8 @@
 import Vue from 'vue'
 import VueYoutube from 'vue-youtube'
 import getYouTubeId from 'get-youtube-id'
-import VueToast from 'vue-toast-notification'
-import 'vue-toast-notification/dist/theme-sugar.css'
 
 Vue.use(VueYoutube)
-Vue.use(VueToast)
 
 export default {
   data() {
@@ -139,7 +136,6 @@ export default {
       clearInterval(this.intervalId)
       this.player.pauseVideo()
       this.playing = false
-      this.$toast.success("Yey! You've created another practice log!")
       this.createPracticeLog()
     },
     setPlaybackRate() {
