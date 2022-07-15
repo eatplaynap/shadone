90ee87859fd5da895e29473fe3a73f00f525df17
eatplaynap <eatplaynap329@gmail.com>
2022-07-15 14:31:09 +0900

    Introduce youtube_helper

---
+++ app/helpers/youtube_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module YoutubeHelper
+  def youtube_iframe_url(url)
+    YouTubeRails.youtube_embed_url_only(url)
+  end
+
+  def youtube_thumbnail_url(url)
+    YouTubeRails.extract_video_image(url)
+  end
+end
