61a774946f4aa6ec5e32796bbcdb410d471292cc
eatplaynap <eatplaynap329@gmail.com>
2022-07-15 14:29:48 +0900

    Remove unnecessary mixin

--- app/javascript/mixins/token.js
+++
@@ -1,8 +0,0 @@
-export default {
-  methods: {
-    token() {
-      const meta = document.querySelector('meta[name="csrf-token"]')
-      return meta ? meta.getAttribute('content') : ''
-    },
-  },
-}
