40d2d063f217ee7001c3e1f13bde72288072f8f9
eatplaynap <eatplaynap329@gmail.com>
2022-07-16 14:41:22 +0900

    rubocop OK

--- spec/factories/practices.rb
+++ spec/factories/practices.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :practice do
     url { 'https://www.youtube.com/watch?v=s3ZX2RX73_g' }
--- spec/system/practices_spec.rb
+++ spec/system/practices_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
+
 # 学習記録の編集・削除
 describe 'practice', type: :system do
-
   it 'has a valid factory' do
     expect(FactoryBot.build(:practice)).to be_valid
   end
