f31570c96385a36aec12d6445292edd156b598c6
eatplaynap <eatplaynap329@gmail.com>
2022-07-17 16:05:17 +0900

    Add user system spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,2 +1,20 @@
 # frozen_string_literal: true
-# ユーザーの削除など
+
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  it 'can show their profile page' do
+    sign_in_as user
+    click_link 'Profile'
+    expect(page).to have_content 'Your Profile'
+  end
+
+  it 'can delete their account' do
+    sign_in_as user
+    click_link 'Profile'
+    click_link 'Delete Account'
+    expect(page).to have_content 'Successfully deleted your account!'
+  end
+end
