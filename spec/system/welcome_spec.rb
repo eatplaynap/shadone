e6f2287704f915126d0214671aac60c73908f102
eatplaynap <eatplaynap329@gmail.com>
2022-07-17 15:50:29 +0900

    Remove unnecessary comment

--- spec/system/welcome_spec.rb
+++ spec/system/welcome_spec.rb
@@ -3,19 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome', type: :system do
-  # it 'cannot access / when user was not logged in' do
-  #   visit root_path
-  #   # /welcomeに戻ることをテスト
-  #   expect(page).to have_content 'Login'
-  # end
-  #
-  # it 'can access / when user was logged in' do
-  #   sign_in_as user
-  #   # ログイン後root_pathにいることをテスト
-  #   visit root_path
-  #   expect(page).to have_content 'Calendar'
-  # end
-
   it 'can do loop' do
   end
 end
