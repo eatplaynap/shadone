eedd135ae88e1922942899f8926adda0e4b5fbd8
eatplaynap <eatplaynap329@gmail.com>
2022-07-18 16:25:45 +0900

    Add request spec of POST a new practice

--- spec/requests/api/practices_spec.rb
+++ spec/requests/api/practices_spec.rb
@@ -1,2 +1,24 @@
 # frozen_string_literal: true
-# practice作成のAPIテスト
+
+require 'rails_helper'
+
+RSpec.describe 'Practice', type: :request do
+  include GoogleOauthMockHelper
+
+  before do
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
+    Rails.application.env_config['omniauth.auth'] = google_oauth_mock
+  end
+
+  it 'POST practice' do
+    get '/auth/google_oauth2/callback'
+    practice = FactoryBot.create(:practice)
+    post api_practices_path, params: {
+      practice: {
+        url: practice.url,
+        duration: practice.duration
+      }
+    }
+    expect(response).to have_http_status(201)
+  end
+end
