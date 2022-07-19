3d5c8dc1f685b8b0e03187b3f1834985db3cdf34
eatplaynap <eatplaynap329@gmail.com>
2022-07-19 16:40:49 +0900

    Commit to check if Net::ReadTimeout error is occurring only on my machine

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
-# 1.ループできることのテスト
-# 2.ループしたら学習記録ができて日付がハイライトされる
-# 3.学習記録ができていれば詳細ページにアクセスできる
-# 4.一時停止できる
+
+require 'rails_helper'
+
+RSpec.describe 'Top', type: :system do
+
+  it 'can do loop', js: true do
+    user = FactoryBot.create(:user)
+    sign_in_as user
+    expect(page).to have_content 'Successfully logged in!'
+  end
+
+  it 'ループしたら学習記録ができて日付がハイライトされる' do
+
+  end
+
+  it '学習記録ができていれば詳細ページにアクセスできる' do
+
+  end
+
+  it '一時停止できる' do
+
+  end
+end
