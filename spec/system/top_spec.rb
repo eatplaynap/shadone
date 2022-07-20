c683f77dcbde21bcf1a67bcfe41a8267a1cda15c
eatplaynap <eatplaynap329@gmail.com>
2022-07-20 10:34:26 +0900

    Add system specs on Top

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -3,22 +3,61 @@
 require 'rails_helper'
 
 RSpec.describe 'Top', type: :system do
+  let(:user) { FactoryBot.create(:user) }
 
-  it 'can do loop', js: true do
-    user = FactoryBot.create(:user)
+  it 'can do loop' do
     sign_in_as user
-    expect(page).to have_content 'Successfully logged in!'
+    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
+    fill_in('start-minutes', with: 1)
+    fill_in('start-seconds', with: 1)
+    fill_in('end-minutes', with: 1)
+    fill_in('end-seconds', with: 2)
+    fill_in('loop-count', with: 3)
+    click_on('Start')
+    sleep 5
+    has_text?('0')
   end
 
-  it 'ループしたら学習記録ができて日付がハイライトされる' do
-
+  it 'can create a new practice log when loop ended' do
+    sign_in_as user
+    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
+    fill_in('start-minutes', with: 1)
+    fill_in('start-seconds', with: 1)
+    fill_in('end-minutes', with: 1)
+    fill_in('end-seconds', with: 2)
+    fill_in('loop-count', with: 3)
+    click_on('Start')
+    sleep 5
+    has_text?("Yey! You've created another practice log!")
   end
 
-  it '学習記録ができていれば詳細ページにアクセスできる' do
-
+  it 'can access a practice show page when a new practice log was created' do
+    sign_in_as user
+    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
+    fill_in('start-minutes', with: 1)
+    fill_in('start-seconds', with: 1)
+    fill_in('end-minutes', with: 1)
+    fill_in('end-seconds', with: 2)
+    fill_in('loop-count', with: 3)
+    click_on('Start')
+    sleep 5
+    scroll_to(:bottom)
+    date_of_today = Time.zone.today.day.to_s
+    click_on(date_of_today)
+    has_text?('Memo')
   end
 
-  it '一時停止できる' do
-
+  it 'can stop middle of the loop and create a practice log' do
+    sign_in_as user
+    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
+    fill_in('start-minutes', with: 1)
+    fill_in('start-seconds', with: 1)
+    fill_in('end-minutes', with: 1)
+    fill_in('end-seconds', with: 2)
+    fill_in('loop-count', with: 10)
+    click_on('Start')
+    sleep 1
+    click_on('Quit')
+    has_text?("Yey! You've created another practice log!")
   end
 end
