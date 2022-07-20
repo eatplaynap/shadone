3079d820fb2290968bb714821e0ecda27a2fe52d
eatplaynap <eatplaynap329@gmail.com>
2022-07-20 10:33:12 +0900

    Changed config to use selenium on all the system specs

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,10 +2,6 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium, using: :headless_chrome
   end
 end
