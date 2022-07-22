4c7fe8ad72cbcb238db5aa7e5014044f47a51c29
eatplaynap <eatplaynap329@gmail.com>
2022-07-22 16:49:58 +0900

    Rewrite to be a valid test

--- spec/system/welcome_spec.rb
+++ spec/system/welcome_spec.rb
@@ -3,16 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome', type: :system do
-  it 'can do loop', js: true do
+  it 'can do loop' do
     visit welcome_path
-    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
-    fill_in('start-minute', with: 1)
-    fill_in('start-second', with: 1)
-    fill_in('end-minute', with: 1)
-    fill_in('end-second', with: 2)
-    fill_in('loop-count', with: 3)
-    click_on('Start')
-    sleep 5
-    has_text?('0')
+    fill_in 'url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g'
+    fill_in 'start-minute', with: 1
+    fill_in 'start-second', with: 1
+    fill_in 'end-minute', with: 1
+    fill_in 'end-second', with: 2
+    fill_in 'loop-count', with: 3
+    click_on 'Start'
+    expect(page).to have_button('Quit')
+    sleep 10
+    expect(page).to have_button('Start')
   end
 end
