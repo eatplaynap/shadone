9698d56ad09076d3dc30d8554b199af91451b69d
eatplaynap <eatplaynap329@gmail.com>
2022-07-22 17:05:34 +0900

    Remove unnecessary code

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -2,17 +2,17 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Session', type: :system do
+RSpec.describe 'Sessions', type: :system do
   let(:user) { FactoryBot.create(:user) }
 
   it 'can log in' do
     sign_in_as user
-    expect(page).to have_content 'Successfully logged in!'
+    expect(page).to have_content('Successfully logged in!')
   end
 
   it 'can log out' do
     sign_in_as user
     click_link 'Logout'
-    expect(page).to have_content 'Successfully logged out!'
+    expect(page).to have_content('Successfully logged out!')
   end
 end
