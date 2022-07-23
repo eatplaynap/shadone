601e967da4f5655daeb67caef3d44771fda7348b
eatplaynap <eatplaynap329@gmail.com>
2022-07-23 15:50:26 +0900

    Combine duplicated tests into one test

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -4,18 +4,24 @@ require 'rails_helper'
 
 RSpec.describe 'Top', type: :system do
   let(:user) { FactoryBot.create(:user) }
+  date_of_today = Time.zone.today.day.to_s
 
-  it 'can do loop' do
+  it 'creates a new practice log and enables to access its show page when loop ended' do
     sign_in_as user
-    fill_in('url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g')
-    fill_in('start-minute', with: 1)
-    fill_in('start-second', with: 1)
-    fill_in('end-minute', with: 1)
-    fill_in('end-second', with: 2)
-    fill_in('loop-count', with: 3)
-    click_on('Start')
+    expect(page).to have_no_link(date_of_today)
+    fill_in 'url', with: 'https://www.youtube.com/watch?v=s3ZX2RX73_g'
+    fill_in 'start-minute', with: 1
+    fill_in 'start-second', with: 1
+    fill_in 'end-minute', with: 1
+    fill_in 'end-second', with: 2
+    fill_in 'loop-count', with: 3
+    click_on 'Start'
+    expect(page).to have_button('Quit')
     sleep 5
-    has_text?('0')
+    expect(page).to have_button('Start')
+    expect(page).to have_content("Yey! You've created another practice log!")
+    click_on date_of_today
+    expect(page).to have_content('Duration')
   end
 
   it 'can create a new practice log when loop ended' do
