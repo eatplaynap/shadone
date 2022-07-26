cc6b31c2d83f9e0c4845f086b7242a5d3a9b377e
eatplaynap <eatplaynap329@gmail.com>
2022-07-26 22:52:19 +0900

    DRY up code and add expectations regards record count

--- spec/system/practice_logs_spec.rb
+++ spec/system/practice_logs_spec.rb
@@ -4,14 +4,27 @@ require 'rails_helper'
 
 RSpec.describe 'PracticeLogs', type: :system do
   let(:user) { FactoryBot.create(:user) }
-  let(:practice_log) { FactoryBot.create(:practice_log, user: user) }
+  let(:practice_log) { FactoryBot.create(:practice_log, memo: 'Lorem ipsum dolor sit amet', practice_on: Date.parse('2022-07-26'), duration: 60, user: user) }
 
-  it 'can edit a practice log' do
+  before do
     sign_in_as user
+  end
+
+  it 'can access practice log show page' do
     visit practice_log_path(practice_log)
-    click_on('Edit')
-    fill_in('Memo', with: 'Edit the practice log.')
-    click_on('Update Practice log')
-    expect(page).to have_content('Edit the practice log.')
+    expect(page).to have_content '2022-07-26'
+    expect(page).to have_content '1 minute'
+    expect(page).to have_content 'Lorem ipsum dolor sit amet'
+  end
+
+  it 'can edit practice log' do
+    visit practice_log_path(practice_log)
+    click_on 'Edit'
+    expect(page).to have_content '2022-07-26'
+    expect(page).to have_content '1 minute'
+    expect(page).to have_content 'Lorem ipsum dolor sit amet'
+    fill_in 'Memo', with: 'Edit the practice log.'
+    click_on 'Update Practice log'
+    expect(page).to have_content 'Edit the practice log.'
   end
 end
