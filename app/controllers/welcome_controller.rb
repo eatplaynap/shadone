821d2666dd0d13cc5aac711f34de0e19afa1d84f
eatplaynap <eatplaynap329@gmail.com>
2022-07-26 23:07:32 +0900

    Change not_login_required as a method and be called at before_action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,13 @@
 
 class WelcomeController < ApplicationController
   skip_before_action :login_required
+  before_action :not_login_required
 
-  def index
+  def index; end
+
+  private
+
+  def not_login_required
     redirect_to root_path if logged_in?
   end
 end
