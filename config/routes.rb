32cb46d121b1432fc0c21d9450b1b2f61040f207
eatplaynap <eatplaynap329@gmail.com>
2022-07-27 20:41:14 +0900

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
   get 'welcome', to: 'welcome#index', as: 'welcome'
+  get 'tos', to: 'welcome#tos', as: 'tos'
+  get 'privacy_policy', to: 'welcome#privacy_policy', as: 'privacy_policy'
   get 'auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resource :me, only: [:show, :destroy], controller: 'users'
