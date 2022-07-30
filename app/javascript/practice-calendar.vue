d3546625062ceaac1235872283730e3591a4e5ec
eatplaynap <eatplaynap329@gmail.com>
2022-07-30 23:25:51 +0900

    Fix design of calendar

--- app/javascript/practice-calendar.vue
+++ app/javascript/practice-calendar.vue
@@ -1,66 +1,68 @@
 <template>
   <div id="practiceCalendar">
-    <div class="card lg:card-side bg-base-100 z-0">
-      <div class="card-body">
-        <div class="card-title">Your Practice Calendar</div>
+    <div class="max-w-2xl mt-6">
+      <div class="bg-base-100 z-0">
+        <div class="">
+          <div class="card-title">Your Practice Calendar</div>
 
-        <div class="navbar bg-base-100 flex justify-center">
-          <div class="normal-case text-xl">
-            <div class="link link-hover">
-              <div class="previous-month" @click="previousMonth">
-                Previous Month
+          <div class="navbar bg-base-100 flex justify-center">
+            <div class="normal-case text-xl">
+              <div @click="previousMonth" class="cursor-pointer w-11 h-11 flex items-center justify-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="sha-icon" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>
               </div>
-            </div>
 
-            <div>{{ calendarYear }} - {{ calendarMonth }}</div>
+              <div>{{ calendarYear }} - {{ calendarMonth }}</div>
 
-            <div class="link link-hover">
-              <div v-if="!newestMonth()" class="next-month" @click="nextMonth">
-                Next Month
+              <div v-if="!newestMonth()" @click="nextMonth" class="cursor-pointer w-11 h-11 flex items-center justify-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="sha-icon" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"/></svg>
               </div>
-              <div v-else class="blank"></div>
+              <div v-else class="blank w-11 h-11 flex items-center justify-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="sha-icon is-disabled" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"/></svg>
+              </div>
             </div>
           </div>
-        </div>
 
-        <table class="border-collapse border border-neutral w-full">
-          <thead class="bg-sub">
-            <tr>
-              <th class="border border-neutral w-1/7">Su</th>
-              <th class="border border-neutral w-1/7">Mo</th>
-              <th class="border border-neutral w-1/7">Tu</th>
-              <th class="border border-neutral w-1/7">We</th>
-              <th class="border border-neutral w-1/7">Th</th>
-              <th class="border border-neutral w-1/7">Fr</th>
-              <th class="border border-neutral w-1/7">Sa</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="week in calendarWeeks" :key="week.id">
-              <td
-                v-for="date in week.value"
-                :key="date.weekDay"
-                :class="[practiceMarkClass(date), todayClass(date)]"
-                class="border border-neutral h-12"
-              >
-                <a v-if="date.id" :href="linkOfPracticeLog(date)">
-                  <div class="text-left inline-block align-top">
-                    {{ date.date }}
+          <table class="border-collapse border border-neutral w-full">
+            <thead class="bg-sub">
+              <tr>
+                <th class="border border-neutral w-1/7">Su</th>
+                <th class="border border-neutral w-1/7">Mo</th>
+                <th class="border border-neutral w-1/7">Tu</th>
+                <th class="border border-neutral w-1/7">We</th>
+                <th class="border border-neutral w-1/7">Th</th>
+                <th class="border border-neutral w-1/7">Fr</th>
+                <th class="border border-neutral w-1/7">Sa</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="week in calendarWeeks" :key="week.id">
+                <td
+                  v-for="date in week.value"
+                  :key="date.weekDay"
+                  :class="[practiceMarkClass(date), todayClass(date)]"
+                  class="border border-neutral h-12 align-top"
+                >
+                  <a v-if="date.id" :href="linkOfPracticeLog(date)" class="sha-date relative block p-1">
+                    <div class="text-left align-top absolute top-0">
+                      {{ date.date }}
+                    </div>
+                    <div class="h-2/3">
+                      <img style="transform: rotate(1deg);" src="/images/done.svg" class="h-10/12" />
+                    </div>
+                  </a>
+                  <div v-else class="sha-date block p-1 relative">
+                    <div class="text-left align-top absolute top-0">
+                      {{ date.date }}
+                    </div>
+                    <div class="h-2/3">
+                       <img src="/images/blank.svg" class="h-10/12" />
+                    </div>
                   </div>
-                  <div class="h-2/3">
-                    <img src="/images/done.svg" class="h-10/12" />
-                  </div>
-                </a>
-                <span v-else>
-                  <div class="text-left inline-block align-top">
-                    {{ date.date }}
-                  </div>
-                  <div class="h-2/3"></div>
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -190,11 +192,3 @@ export default {
   },
 }
 </script>
-
-<style>
-/*delete when time comes*/
-
-.is-today {
-  border: dashed 2px;
-}
-</style>
