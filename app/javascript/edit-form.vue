8697c44c5242b47fa495995dbad1ed63f7eebb8e
eatplaynap <eatplaynap329@gmail.com>
2022-08-31 09:44:42 +0900

    To fix failing test

--- app/javascript/edit-form.vue
+++ app/javascript/edit-form.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-     <div class="flex items-center">
+    <div class="flex items-center">
       <div class="w-24">
         <label for="memo" class="font-bold">Memo</label>
       </div>
+
       <div class="flex-1">
+        <!--          <p>{{ practiceLogMemo }}</p>-->
         <div class="sha-pre-wrap" v-text="practiceLogMemo" />
       </div>
     </div>
@@ -17,6 +19,7 @@
         Add Memo
       </button>
     </div>
+
     <div v-else>
       <textarea
         id="memo"
