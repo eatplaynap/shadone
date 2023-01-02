e354ad3addfbcd217660649794c436f69e90739b
eatplaynap <eatplaynap329@gmail.com>
2023-01-02 16:39:39 +0900

    add: dummy test

--- spec/javascripts/after-login-main.spec.js
+++ spec/javascripts/after-login-main.spec.js
@@ -1,1 +1,6 @@
-import AfterLoginMain from "../../app/javascript/after-login-main.vue";
+import AfterLoginMain from "../../app/javascript/after-login-main.vue"
+
+// TODO: Add tests for the AfterLoginMain component
+test('AfterLoginMain', () => {
+    expect('fuga').not.toMatch('hoge');
+});
