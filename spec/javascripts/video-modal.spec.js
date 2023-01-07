1291c384fb3f40d5813e0cfc356f4afe521e5dc4
eatplaynap <eatplaynap329@gmail.com>
2023-01-07 21:30:43 +0900

    add: modal open test which is not working

--- spec/javascripts/video-modal.spec.js
+++ spec/javascripts/video-modal.spec.js
@@ -1,11 +1,19 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount,mount, createLocalVue } from '@vue/test-utils'
+import VModal from 'vue-js-modal/dist/index.nocss.js'
 import VideoModal from '../../app/javascript/video-modal.vue'
 
-// TODO: Add test
-
 describe('VideoModal', () => {
-    it('is a Vue instance', () => {
-        const wrapper = mount(VideoModal)
-        expect(wrapper.vm).toBeTruthy()
+    it('opens a modal when clicked on How To Use button', async () => {
+        const localVue = createLocalVue()
+        localVue.use(VModal)
+        const wrapper = mount(VideoModal, {
+            localVue
+        })
+        expect(wrapper.get(`[data-testid="instruction-video-modal"]`).exists()).not.toBe(true)
+        await wrapper.get(`[data-testid="how-to-use-button"]`).trigger('click')
+        expect(wrapper.get(`[data-testid="instruction-video-modal"]`).exists()).toBe(true)
+    })
+    it('closes a modal', () => {
+
     })
 })
