ab97148af29cce7a889eea06d799da4c7f1d63ee
eatplaynap <eatplaynap329@gmail.com>
2023-01-10 21:43:14 +0900

    Refactor test

--- spec/javascripts/video-modal.spec.js
+++ spec/javascripts/video-modal.spec.js
@@ -3,10 +3,13 @@ import VModal from 'vue-js-modal/dist/index.nocss.js'
 import VideoModal from '../../app/javascript/video-modal.vue'
 
 describe('VideoModal', () => {
-    const localVue = createLocalVue()
-    localVue.use(VModal)
-    const wrapper = mount(VideoModal, {
-        localVue
+    let wrapper
+    beforeEach(() => {
+        const localVue = createLocalVue()
+        localVue.use(VModal)
+        wrapper = mount(VideoModal, {
+            localVue
+        })
     })
 
     it('opens a modal when clicked on How To Use button', async () => {
@@ -15,18 +18,8 @@ describe('VideoModal', () => {
         expect(wrapper.get(`[data-testid="instruction-video-modal"]`).exists()).toBe(true)
     })
     it('closes a modal when clicked on outside of iframe', async () => {
-        await wrapper.get(`[data-testid="how-to-use-button"]`).trigger('click')
-        const videoModalContainer = wrapper.get(`[data-testid="video-modal-container"]`).html()
-        console.log(videoModalContainer)
-        const outside = videoModalContainer.getElementById('app').closest(":not(iframe)")
-        console.log(outside)
-        await wrapper.get(`[data-testid="instruction-video-modal"]`).trigger('click')
-        expect(wrapper.find(`[data-testid="instruction-video-modal"]`).exists()).not.toBe(true)
-
-
-        // await wrapper.get(`[data-testid="how-to-use-button"]`).trigger('click')
-        // const outSide = wrapper.get('iframe')
-        // await wrapper.find(outSide).trigger('click')
-        // expect(wrapper.find(`[data-testid="instruction-video-modal"]`).exists()).not.toBe(true)
+        // click on How To Use button
+        // ensures that the modal is open
+        // click on outside of iframe
     })
 })
